dff4d0c98c622b8bc3f5ce4deb288d6c8588eba7
Josh Pigford <josh@joshpigford.com>
2019-06-11 11:01:54 -0500

    Omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
-class ApplicationController < ActionController::Base
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
+  
+  def sign_in_with(provider_name)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user, :event => :authentication
+    set_flash_message(:notice, :success, :kind => provider_name) if is_navigational_format?
+  end
+
+  def facebook
+    sign_in_with "Spotify"
+  end
+
+  def linkedin
+    sign_in_with "LinkedIn"
+  end
+
+  def twitter
+    sign_in_with "Twitter"
+  end
+
+  def google_oauth2
+    sign_in_with "Google"
+  end
+
+  def developer
+    sign_in_with "Developer"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks" 
+  }
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  
   root to: "home#index"
 end
