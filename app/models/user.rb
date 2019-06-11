88710ea5bf4a80573934f8b6e058fccc17ef3cd7
Josh Pigford <josh@joshpigford.com>
2019-06-11 10:38:18 -0500

    Init

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :rememberable, :trackable, :omniauthable, omniauth_providers: %i[spotify]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+    end
+  end
+end
