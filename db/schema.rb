74498242b739295afaebc523e623e02123de89a1
Josh Pigford <josh@joshpigford.com>
2019-06-12 08:20:10 -0500

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -17,18 +17,17 @@ ActiveRecord::Schema.define(version: 2019_06_11_202850) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "provider"
     t.string "uid"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "settings"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
