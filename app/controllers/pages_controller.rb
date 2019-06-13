f075f134b55f55e1df49cd035f2cb26cc28c3bbf
Josh Pigford <josh@joshpigford.com>
2019-06-13 09:16:33 -0500

    Logged in view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  def index
+    if current_user
+      @latest_streams = current_user.streams
+    end
+  end
   def home
     
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
   has_many :tracks, through: :follows
   has_many :albums, through: :tracks
   has_many :artists, through: :tracks
+  has_many :streams
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
