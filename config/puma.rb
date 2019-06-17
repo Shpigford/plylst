df630ee66c1ce2dc3bf44f11437b9de602b3da9e
Josh Pigford <josh@joshpigford.com>
2019-06-16 20:08:33 -0500

    No

--- config/puma.rb
+++ config/puma.rb
@@ -18,5 +18,4 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-  Barnes.start
 end
