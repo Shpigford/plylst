66d0a8a78b0bb537d5aa73a03f6718c3faaa7a6a
Josh Pigford <josh@joshpigford.com>
2019-06-17 13:15:56 -0500

    Order streams by play time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
     if current_user
-      @latest_streams = current_user.streams.limit(50)
+      @latest_streams = current_user.streams.limit(50).order('played_at DESC')
     end
   end
   def home
