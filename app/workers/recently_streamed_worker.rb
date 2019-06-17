c09579f7e368b229a36efda7647fe415cae14dec
Josh Pigford <josh@joshpigford.com>
2019-06-17 11:02:53 -0500

    Start saving streams

--- app/workers/recently_streamed_worker.rb
+++ app/workers/recently_streamed_worker.rb
@@ -1,18 +1,25 @@
 class RecentlyStreamedWorker
   include Sidekiq::Worker
 
-  def perform(user_id)
-    user = User.find user_id
-    connection = user.settings.to_hash
-    spotify = RSpotify::User.new(connection)
-    recent_tracks = spotify.recently_played(limit: 50)
+  def perform
 
-    recent_track_ids = Array.new
+    User.all.each do |user|
+      user = User.find user.id
+      connection = user.settings.to_hash
+      spotify = RSpotify::User.new(connection)
+      recent_tracks = spotify.recently_played(limit: 50)
 
-    recent_tracks.each do |track|
-      recent_track_ids.push([track.id, track.played_at])
+      recent_track_ids = Array.new
+
+      recent_tracks.each do |track|
+        recent_track_ids.push([track.id, track.played_at])
+      end
+
+      SaveTracksWorker.perform_async(user.id, recent_track_ids, 'streamed')
     end
 
-    SaveTracksWorker.perform_async(user.id, recent_track_ids, 'streamed') 
   end
 end
+
+
+# Sidekiq::Cron::Job.create(name: 'Recently Streamed', cron: '0 */2 * * *', class: 'RecentlyStreamedWorker')
