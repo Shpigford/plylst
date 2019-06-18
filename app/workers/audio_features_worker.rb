01aa479528ef0c8d49999dc31ff44a375366ca74
Josh Pigford <josh@joshpigford.com>
2019-06-18 13:20:02 -0500

    Make sure there's actually data from Spotify

--- app/workers/audio_features_worker.rb
+++ app/workers/audio_features_worker.rb
@@ -5,21 +5,23 @@ class AudioFeaturesWorker
     spotify_tracks = RSpotify::AudioFeatures.find(track_ids)
 
     spotify_tracks.each do |spotify_track|
-      track = Track.find_by(spotify_id: spotify_track.id)
-      track.update_attribute(:audio_features, {
-        acousticness: spotify_track.acousticness,
-        danceability: spotify_track.danceability,
-        energy: spotify_track.energy,
-        instrumentalness: spotify_track.instrumentalness,
-        key: spotify_track.key,
-        liveness: spotify_track.liveness,
-        loudness: spotify_track.loudness,
-        mode: spotify_track.mode,
-        speechiness: spotify_track.speechiness,
-        tempo: spotify_track.tempo,
-        time_signature: spotify_track.time_signature,
-        valence: spotify_track.valence
-      })
+      if spotify_track.present?
+        track = Track.find_by(spotify_id: spotify_track.id)
+        track.update_attribute(:audio_features, {
+          acousticness: spotify_track.acousticness,
+          danceability: spotify_track.danceability,
+          energy: spotify_track.energy,
+          instrumentalness: spotify_track.instrumentalness,
+          key: spotify_track.key,
+          liveness: spotify_track.liveness,
+          loudness: spotify_track.loudness,
+          mode: spotify_track.mode,
+          speechiness: spotify_track.speechiness,
+          tempo: spotify_track.tempo,
+          time_signature: spotify_track.time_signature,
+          valence: spotify_track.valence
+        })
+      end
     end
   end
 end
