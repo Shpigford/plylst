a48808b381ac69c41bc982a805a4e53a0a0f5ad9
Josh Pigford <josh@joshpigford.com>
2019-06-18 20:31:51 -0500

    Better variable formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,45 @@ module ApplicationHelper
   end
 
   def human_readable(variable_hash)
-    output = variable_hash.delete_if { |k, v| v.blank? }.map {|k,v| "#{k.split('_').map(&:capitalize).join(' ')}: #{v.to_s.split('_').map(&:capitalize).join(' ')}"}.join(' - ')
+    filter_map = {
+      "lt" => "Less than",
+      "gt" => "Greater than",
+    }
+
+    musical_key_map = {
+      0 => "C",
+      1 => "C♯, D♭",
+      2 => "D",
+      3 => "D♯, E♭",
+      4 => "E",
+      5 => "F",
+      6 => "F♯, G♭",
+      7 => "G",
+      8 => "G♯, A♭",
+      9 => "A",
+      10 => "A♯, B♭",
+      11 => "B",
+    }
+
+    danceability_map = {
+      0 => "Not at all",
+      1 => "A little",
+      2 => "Somewhat",
+      3 => "Moderately",
+      4 => "Very",
+      5 => "Super",
+    }
+
+    output = variable_hash.delete_if { |k, v| v.blank? }.map do |key, value| 
+      label = "#{key.split('_').map(&:capitalize).join(' ')}"
+      formatted_value = case key
+      when "days_ago_filter", "plays_filter", "bpm_filter", "duration_filter" then filter_map.fetch(value)
+      when "key" then musical_key_map.fetch(value)
+      when "danceability" then danceability_map.fetch(value)
+      else value.to_s.split('_').map(&:capitalize).join(' ')
+      end
+      "#{label}: #{formatted_value}" 
+    end.join(' - ')
 
     if output.blank?
       output = "No variables set"
