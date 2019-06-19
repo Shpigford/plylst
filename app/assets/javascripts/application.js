1184836b806c1501b48e56c2da28f5ef93cf6a8b
Josh Pigford <josh@joshpigford.com>
2019-06-19 14:07:42 -0500

    More bits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -129,56 +129,14 @@ $(document).ready(function() {
         label: 'Release Date',
         type: 'date',
         operators: ['between']
-      },
-      {
-        id: 'category',
-        label: 'Category',
-        type: 'integer',
-        input: 'select',
-        values: {
-          1: 'Books',
-          2: 'Movies',
-          3: 'Music',
-          4: 'Tools',
-          5: 'Goodies',
-          6: 'Clothes'
-        },
-        operators: ['equal', 'not_equal', 'in', 'not_in', 'is_null', 'is_not_null']
-      }, 
-      {
-        id: 'in_stock',
-        label: 'In stock',
-        type: 'integer',
-        input: 'radio',
-        values: {
-          1: 'Yes',
-          0: 'No'
-        },
-        operators: ['equal']
-      }, 
-      {
-        id: 'price',
-        label: 'Price',
-        type: 'double',
-        validation: {
-          min: 0,
-          step: 0.01
-        }
-      }, 
-      {
-        id: 'id',
-        label: 'Identifier',
-        type: 'string',
-        placeholder: '____-____-____',
-        operators: ['equal', 'not_equal'],
-        validation: {
-          format: /^.{4}-.{4}-.{4}$/
-        }
       }
     ],
     /*rules: rules_basic,*/
     allow_groups: 0,
     conditions: ['AND'],
+    sort_filters: true,
+    inputs_separator: 'and',
+    select_placeholder: ' ',
     icons: {
       add_group: 'fas fa-plus-square',
       add_rule: 'fas fa-plus',
