d58b74f4bff8123b397ed86c1320672cae1bde85
Josh Pigford <josh@joshpigford.com>
2019-07-01 14:06:47 -0500

    Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,7 @@
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.environments = ['production']
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
+end
