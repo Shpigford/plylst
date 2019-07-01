d05e990c2ad881ecd278a012f9d57d74bd1baf59
Josh Pigford <josh@joshpigford.com>
2019-07-01 14:16:25 -0500

    Ignore that exception

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,5 +3,6 @@ if Rails.env.production?
     config.dsn = ENV['SENTRY_DSN']
     config.environments = ['production']
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.excluded_exceptions += ['RestClient::TooManyRequests']
   end
 end
