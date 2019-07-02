7fad90c23c45459678e979da44f5a72becc45864
Josh Pigford <josh@joshpigford.com>
2019-07-01 22:39:12 -0500

    Handle permission eror

--- app/workers/process_account_worker.rb
+++ app/workers/process_account_worker.rb
@@ -10,14 +10,22 @@ class ProcessAccountWorker
       track_ids = Array.new
       
       (0..1000000000).step(50) do |n|
-        tracks = spotify.saved_tracks(limit: 50, offset: n)
-        tracks_added_at = spotify.tracks_added_at
-        break if tracks.size == 0
+        begin
+          tracks = spotify.saved_tracks(limit: 50, offset: n)
+        rescue RestClient::Forbidden => e
+          # Deactivate user if we don't have the right permissions
+          user.update_attribute(:active, false)
+        end
 
-        SaveTracksWorker.perform_async(user.id, tracks_added_at.to_a, 'added')
+        if tracks.present?
+          tracks_added_at = spotify.tracks_added_at
+          break if tracks.size == 0
+
+          SaveTracksWorker.perform_async(user.id, tracks_added_at.to_a, 'added')
+        end
       end
 
-      UserDataUpdateWorker.perform_async(user.id)
+      UserDataUpdateWorker.perform_async(user.id) if user.active?
     end
   end
 end
