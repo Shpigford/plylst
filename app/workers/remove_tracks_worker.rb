2578bb2e256c523e05bdb04dd675a0f8bd6f508a
Josh Pigford <josh@joshpigford.com>
2019-07-01 21:08:18 -0500

    Handle permission errors

--- app/workers/remove_tracks_worker.rb
+++ app/workers/remove_tracks_worker.rb
@@ -6,16 +6,26 @@ class RemoveTracksWorker
     connection = user.settings.to_hash
     spotify = RSpotify::User.new(connection)
 
-    saved = spotify.saved_tracks?(track_ids)
+    if user.active?
+      begin
+        saved = spotify.saved_tracks?(track_ids)
+      rescue RestClient::Forbidden => e
+        # Deactivate user if we don't have the right permissions
+        user.update_attribute(:active, false)
+      end
 
-    track_saved = Hash[track_ids.zip(saved)]
+      if saved.present?
+        track_saved = Hash[track_ids.zip(saved)]
 
-    removed_tracks = track_saved.delete_if { |k,v| v === true}
+        removed_tracks = track_saved.delete_if { |k,v| v === true}
 
-    removed_tracks.each do |removed_track|
-      track = Track.find_by(spotify_id: removed_track[0])
-      
-      user.follows.find_by(track: track).update_attribute(:active, false)
+        removed_tracks.each do |removed_track|
+          track = Track.find_by(spotify_id: removed_track[0])
+          
+          user.follows.find_by(track: track).update_attribute(:active, false)
+        end
+      end
     end
+
   end
 end
