5618caa1f00049571ec3f444a9cbc8dfc47a29aa
Josh Pigford <josh@joshpigford.com>
2019-07-03 15:00:27 -0500

    Annotate this beast

--- app/workers/save_tracks_worker.rb
+++ app/workers/save_tracks_worker.rb
@@ -2,37 +2,50 @@ class SaveTracksWorker
   include Sidekiq::Worker
 
   def perform(user_id, tracks_with_date, kind = 'added')
+    # Find user these tracks are associated with, if a user_id is passed
     user = User.find user_id if user_id.present?
 
+    # Check if `tracks_with_date` is actually a multi-dimensional array (meaning it has dates)
     if tracks_with_date.all? { |e| e.kind_of? Array }
       track_ids = tracks_with_date.map(&:first)
     else
       track_ids = tracks_with_date
     end
 
+    # Make the Spotify API call to get all of the tracks
     spotify_tracks = RSpotify::Track.find(track_ids)
 
+    # Looop through the returned tracks
     spotify_tracks.each do |spotify_track|
+
+      # Search to see if the track already exists, if it does not, initialize a new object based on the Spotify ID
       track = Track.where(spotify_id: spotify_track.id).first_or_initialize(spotify_id: spotify_track.id)
 
+      # If it's a new track record, do this
       if track.new_record?
         spotify_artist = spotify_track.artists.first
+
+        # Search to see if the artist for the track already exists, if it does not, initialize a new object
         artist = Artist.where(spotify_id: spotify_artist.id).first_or_initialize(spotify_id: spotify_artist.id)
 
+        # If it's a new artist record, save the initialized new object and spin off a separate worker to build the artist
         if artist.new_record?
           artist.save
           BuildArtistWorker.perform_async(artist.id)
         end
 
+        # Search to see if the album for the track already exists, if it does not, initialize a new object
         spotify_album = spotify_track.album
         album = Album.where(spotify_id: spotify_album.id).first_or_initialize(spotify_id: spotify_album.id)
 
+        # If it's a new album record, save the initialized new object and spin off a separate worker to build the album
         if album.new_record?
           album.artist = artist
           album.save
           BuildAlbumWorker.perform_async(album.id)
         end
 
+        # Fill in the data for the track and save it
         track.artist = artist
         track.album = album
         track.duration = spotify_track.duration_ms
@@ -42,15 +55,17 @@ class SaveTracksWorker
         track.popularity = spotify_track.popularity
         track.preview_url = spotify_track.preview_url
         track.save
-
-        #GetLyricsWorker.perform_async(track.id) if ENV['genius']
       end
 
+      # If this worker was called with 'added', we're adding these tracks to the User's library as a track they have saved/followed
+      # So, we need to check for that in the Follow table and update accordingly
       if kind == 'added'
         user.tracks << track unless Follow.where(user: user, track: track).present?
         follow = Follow.where(user: user, track: track).first
         added_at = tracks_with_date.select{|(x, y)| x == spotify_track.id}.first[1].to_time
         follow.update_attribute(:added_at, added_at)
+
+      # If this track was created from the "RecentlyStreamedWorker" worker, be sure to add that stream
       elsif kind == 'streamed'
         follow = Follow.where(user: user, track: track).first
 
@@ -69,6 +84,7 @@ class SaveTracksWorker
       end
     end
 
+    # Build the "audio features" for the tracks
     AudioFeaturesWorker.perform_async(track_ids)
   end
 end
