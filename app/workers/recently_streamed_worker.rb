ba76597ac805dc5f649e11f606d0224e0e4fdd3b
Josh Pigford <josh@joshpigford.com>
2019-07-04 17:44:22 -0500

    Move to default queue instead of slow

--- app/workers/recently_streamed_worker.rb
+++ app/workers/recently_streamed_worker.rb
@@ -1,7 +1,7 @@
 class RecentlyStreamedWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :slow, lock: :while_executing
+  sidekiq_options lock: :while_executing
 
   def perform(user_id)
     user = User.find user_id
