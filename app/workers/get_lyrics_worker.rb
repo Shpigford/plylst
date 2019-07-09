f8373e56be39a60318355b6977dd80beabbe4cdb
Josh Pigford <josh@joshpigford.com>
2019-07-09 09:37:28 -0500

    Make sure tracks have artist names

--- app/workers/get_lyrics_worker.rb
+++ app/workers/get_lyrics_worker.rb
@@ -12,7 +12,7 @@ class GetLyricsWorker
   def perform(track_id)
     track = Track.find(track_id)
 
-    if track.present? and track.lyrics == nil
+    if track.present? and track.lyrics == nil and track.artist.name.present?
       songs = Genius::Song.search("#{track.name} by #{track.artist.name}")
       if songs.present?
         if songs.first.primary_artist.name.downcase == track.artist.name.downcase
