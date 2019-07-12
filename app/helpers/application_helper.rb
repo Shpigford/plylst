5bed3e8f195bb812671d2a35152177f630ee1f76
Josh Pigford <josh@joshpigford.com>
2019-07-12 15:43:10 -0500

    Better tooltip mapping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,12 +41,27 @@ module ApplicationHelper
       3 => "Insane",
     }
 
+    acoustic_map = {
+      0 => "Not at all",
+      1 => "Somewhat",
+      2 => "Likely",
+      3 => "Very Likely",
+    }
+
     valence_map = {
       0 => "Negative",
       1 => "Positive",
-      2 => "NIL"
     }
 
+    speech_map = {
+      0 => "No",
+      1 => "Yes",
+    }
+
+    instrumentalness_map = {
+      0 => "No",
+      1 => "Yes",
+    }
 
     danceability_map = {
       0 => "Not at all",
@@ -57,14 +72,18 @@ module ApplicationHelper
       5 => "Super",
     }
 
-    output = variable_hash.delete_if { |k, v| v.blank? }.map do |key, value| 
-      label = "#{key.split('_').map(&:capitalize).join(' ').gsub('Tempo', 'BPM').gsub('Valence', 'Mood')}"
+    output = variable_hash.delete_if{|k| k == 'liveness' || k == 'loudness' || k == 'speechiness' || k == 'time_signature' }.delete_if { |k, v| v.blank? }.map do |key, value| 
+      label = "#{key.split('_').map(&:capitalize).join(' ').gsub('Tempo', 'BPM').gsub('Instrumentalness', 'Instrumental').gsub('Tempo', 'BPM').gsub('Valence', 'Mood')}"
       formatted_value = case key
       when "key" then musical_key_map.fetch(value)
       when "mode" then mode_map.fetch(value)
       when "tempo" then value.to_i
       when "energy" then energy_map.fetch(energy_value(value))
       when "valence" then valence_map.fetch(valence_value(value))
+      when "speechiness" then speech_map.fetch(speech_value(value))
+      when "acousticness" then acoustic_map.fetch(acoustic_value(value))
+      when "danceability" then danceability_map.fetch(danceability_value(value))
+      when "instrumentalness" then instrumentalness_map.fetch(instrumentalness_value(value))
       #when "days_ago_filter", "plays_filter", "bpm_filter", "duration_filter" then filter_map.fetch(value)
       #when "danceability" then danceability_map.fetch(value)
       #else value.to_s.split('_').map(&:capitalize).join(' ')
@@ -89,11 +108,44 @@ module ApplicationHelper
     end
   end
 
+  def acoustic_value(value)
+    case value
+    when 0.0..0.250 then 0
+    when 0.251..0.500 then 1
+    when 0.501..0.750 then 2
+    when 0.751..1.0 then 3
+    end
+  end
+
+  def danceability_value(value)
+    case value
+    when 0.0..0.199 then 0
+    when 0.2..0.399 then 1
+    when 0.4..0.599 then 2
+    when 0.6..0.799 then 3
+    when 0.8..0.899 then 4
+    when 0.9..1.0 then 5
+    end
+  end
+
   def valence_value(value)
     case value
     when 0.0..0.5 then 0
     when 0.51..1.0 then 1
-    else 2
+    end
+  end
+
+  def instrumentalness_value(value)
+    case value
+    when 0.0..0.799 then 0
+    when 0.8..1.0 then 1
+    end
+  end
+
+  def speech_value(value)
+    case value
+    when 0.0..0.66 then 0
+    when 0.667..1.0 then 1
     end
   end
 end
