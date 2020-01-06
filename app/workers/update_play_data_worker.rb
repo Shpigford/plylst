fd90a95e263491b50f9050d413237cb16569af56
Josh Pigford <josh@joshpigford.com>
2020-01-06 15:14:51 -0600

    Maybe doing this directly with SQL solves all the world's problems

--- app/workers/update_play_data_worker.rb
+++ app/workers/update_play_data_worker.rb
@@ -5,13 +5,19 @@ class UpdatePlayDataWorker
     user = User.find user_id
 
     if user.active?
-      user.follows.find_each do |follow|
-        if follow.streams.exists?
-          follow.plays = follow.streams.count
-          follow.last_played_at = follow.streams.maximum(:played_at)
-          follow.save
-        end
-      end
+      ActiveRecord::Base.connection.exec_query("UPDATE follows 
+        SET plays = s.plays, last_played_at = s.last 
+        FROM (
+          SELECT 
+            track_id, 
+            count(1) as plays, 
+            max(played_at) as last 
+          FROM streams 
+          WHERE user_id = #{user.id}
+          GROUP BY track_id
+        ) s 
+        WHERE s.track_id = follows.track_id 
+          AND follows.user_id = #{user.id}")
     end
   end
 end
