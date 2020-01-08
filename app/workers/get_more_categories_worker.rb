a73c7712db2ef5210ef285f66e1062e35f70314a
Josh Pigford <josh@joshpigford.com>
2020-01-08 15:30:06 -0600

    Rescue a playlist category coming back 404

--- app/workers/get_more_categories_worker.rb
+++ app/workers/get_more_categories_worker.rb
@@ -6,8 +6,11 @@ class GetMoreCategoriesWorker
   def perform
     # Categories
     RSpotify::Category.list(limit:50).each do |category|
-      playlists = category.playlists(limit:50)
-
+      begin
+        playlists = category.playlists(limit:50)
+      rescue RestClient::NotFound => e
+      end
+      
       if playlists.present?
         playlists.each do |playlist|
           GetMorePlaylistsWorker.perform_async(playlist.owner.id, playlist.id)
