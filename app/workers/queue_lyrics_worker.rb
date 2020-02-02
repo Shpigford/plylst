9ea477b8ad2c2155168d116f88e2203a1c1a2198
Josh Pigford <josh@joshpigford.com>
2020-02-01 18:43:53 -0600

    Try to make the dang lyrics queue work

--- app/workers/queue_lyrics_worker.rb
+++ app/workers/queue_lyrics_worker.rb
@@ -1,12 +1,12 @@
 class QueueLyricsWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :slow
+  sidekiq_options queue: :lyrics
 
   def perform
     Track.where(lyrics: nil).find_each do |track|
       if track.lyrics.blank?
-        GetLyricsWorker.perform_async(track.id) if track.lyrics_last_checked_at.blank? or track.lyrics_last_checked_at < 90.days.ago
+        GetLyricsWorker.set(queue: :lyrics).perform_async(track.id) if track.lyrics_last_checked_at.blank? or track.lyrics_last_checked_at < 90.days.ago
       end
     end
   end
