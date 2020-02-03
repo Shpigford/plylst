2d42004f5575bce9e1c031cac69b2c814f330085
Josh Pigford <josh@joshpigford.com>
2020-02-03 13:01:07 -0600

    Hide sidebar on genres page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,5 +11,6 @@ class PagesController < ApplicationController
 
   def genres
     @genres = Artist.pluck(:genres).reject!(&:empty?).flatten.group_by(&:itself).map { |k,v| [k, v.count] }.to_h.sort_by{|k,v| v}.reverse
+    @hide_sidebar = true
   end
 end
