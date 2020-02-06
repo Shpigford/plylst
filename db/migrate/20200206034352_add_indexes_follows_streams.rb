e5aa602c18c6e1dcb615eaecff516a1c542d2ba3
Josh Pigford <josh@joshpigford.com>
2020-02-06 10:41:27 -0600

    Need to batch this action by user

--- db/migrate/20200206034352_add_indexes_follows_streams.rb
+++ db/migrate/20200206034352_add_indexes_follows_streams.rb
@@ -1,13 +1,15 @@
 class AddIndexesFollowsStreams < ActiveRecord::Migration[5.2]
   def change
-    grouped = Follow.all.order('updated_at DESC').group_by{|model| [model.user_id,model.track_id] }
+    User.all.find_each do |user|
+      grouped = Follow.where(user_id: user.id).order('updated_at DESC').group_by{|model| [model.user_id,model.track_id] }
 
-    grouped.values.each do |duplicates|
-      # the first one we want to keep right?
-      first_one = duplicates.shift # or pop for last one
-      # if there are any more left, they are duplicates
-      # so delete all of them
-      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+      grouped.values.each do |duplicates|
+        # the first one we want to keep right?
+        first_one = duplicates.shift # or pop for last one
+        # if there are any more left, they are duplicates
+        # so delete all of them
+        duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+      end
     end
 
     remove_index :follows, name: "index_follows_on_user_id_and_track_id"
