1844d93061f06b8fc782c234f4fdbe3b3a667c85
Scott Robertson <scottymeuk@gmail.com>
2020-02-06 22:20:45 +0000

    Make use of ar-import, and fetch all artists and albums in one go at the top
    Need to make use of AR imports for albums and artists too, but that will mean looping over things twice, and i am trying to change as little as possible here for now

--- app/workers/save_tracks_worker.rb
+++ app/workers/save_tracks_worker.rb
@@ -17,53 +17,49 @@ class SaveTracksWorker
     existing_ids = Track.where(spotify_id: track_ids).pluck(:spotify_id)
     missing_ids = (track_ids - existing_ids).uniq
 
+    tracks_to_save = {}
+
     # If there are no missing IDs, STOP THAT JUNK
     if missing_ids.present?
       # Make the Spotify API call to get all of the tracks
       spotify_tracks = RSpotify::Track.find(missing_ids)
+      spotify_artist_by_track = spotify_tracks.map{|t| [t.id, t.artists.first] }.to_h
+      spotify_album_by_track = spotify_tracks.map{|t| [t.id, t.album] }.to_h
+
+      artists = Artist.where(spotify_id: spotify_artist_by_track.values.flatten.map(&:id)).map{|a| [a.spotify_id, a] }.to_h
+      albums = Album.where(spotify_id: spotify_album_by_track.values.flatten.map(&:id)).map{|a| [a.spotify_id, a] }.to_h
 
       # Looop through the returned tracks
       spotify_tracks.each do |spotify_track|
         # Initialize a new object based on the Spotify ID
         track = Track.new(spotify_id: spotify_track.id)
-        
-        spotify_artist = spotify_track.artists.first
         audio_feature_ids.push(spotify_track.id)
 
         # Search to see if the artist for the track already exists, if it does not, initialize a new object
-        artist = Artist.where(spotify_id: spotify_artist.id).first_or_initialize(spotify_id: spotify_artist.id)
-
-        # If it's a new artist record, save the initialized new object and spin off a separate worker to build the artist
-        if artist.new_record?
-          artist.save
-          BuildArtistWorker.perform_async(artist.id)
-        end
+        # TODO: Save these in bulk too
+        artists[spotify_track.artists.first.id] ||= Artist.create!(spotify_id: spotify_track.artists.first.id)
+        BuildArtistWorker.perform_async(artist.id) if artists[spotify_track.artists.first.id].new_record?
 
         # Search to see if the album for the track already exists, if it does not, initialize a new object
-        spotify_album = spotify_track.album
-        album = Album.where(spotify_id: spotify_album.id).first_or_initialize(spotify_id: spotify_album.id)
-
-        # If it's a new album record, save the initialized new object and spin off a separate worker to build the album
-        if album.new_record?
-          album.artist = artist
-          album.save
-          BuildAlbumWorker.perform_async(album.id)
-        end
+        # TODO: Save these in bulk too
+        albums[spotify_track.album.id] ||= Album.create!(spotify_id: spotify_track.album.id, artist: artists[spotify_track.artists.first.id])
+        BuildAlbumWorker.perform_async(album.id) if albums[spotify_track.album.id].new_record?
 
         # Fill in the data for the track and save it
-        track.artist = artist
-        track.album = album
+        track.artist = artists[spotify_track.artists.first.id]
+        track.album = albums[spotify_track.album.id]
         track.duration = spotify_track.duration_ms
         track.explicit = spotify_track.explicit
         track.link = spotify_track.external_urls['spotify']
         track.name = spotify_track.name
         track.popularity = spotify_track.popularity
         track.preview_url = spotify_track.preview_url
-        track.save
 
+        tracks_to_save[spotify_track.id] = track
       end
     end
 
+    Track.import! tracks_to_save.values, on_duplicate_key_update: {conflict_target: [:spotify_id], columns: []}
 
     # If this worker was called with 'added', we're adding these tracks to the User's library as a track they have saved/followed
     # So, we need to check for that in the Follow table and update accordingly
@@ -71,6 +67,7 @@ class SaveTracksWorker
       # Make the Spotify API call to get all of the tracks
       spotify_tracks = RSpotify::Track.find(track_ids)
       tracks = Track.where(spotify_id: track_ids)
+      follows = []
 
       # Looop through the returned tracks
       spotify_tracks.each do |spotify_track|
@@ -79,11 +76,13 @@ class SaveTracksWorker
         if track.present?
           begin
             added_at = tracks_with_date.select{|(x, y)| x == spotify_track.id}.first[1].to_time
-            Follow.create(user: user, track: track, added_at: added_at)
+            follows < Follow.new(user: user, track: track, added_at: added_at)
           rescue
           end
         end
       end
+
+      Follow.import follows, on_duplicate_key_update: {conflict_target: [:user_id, :track_id], columns: []}
     end
 
 
@@ -92,6 +91,7 @@ class SaveTracksWorker
       # Make the Spotify API call to get all of the tracks
       spotify_tracks = RSpotify::Track.find(track_ids)
       tracks = Track.where(spotify_id: track_ids)
+      streams = []
 
       # Looop through the returned tracks
       spotify_tracks.each do |spotify_track|
@@ -102,10 +102,13 @@ class SaveTracksWorker
 
           streams.each do |stream|
             time = stream[1].to_time
-            Stream.create(user: user, track: track, played_at: time)
+            streams << Stream.new(user: user, track: track, played_at: time)
           end
         end
       end
+
+      Stream.import streams, on_duplicate_key_update: {conflict_target: [:user_id, :track_id, :played_at], columns: []}
     end
+
   end
 end
