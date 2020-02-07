8bc34309886435a76cd0df79e67df3c437726394
Josh Pigford <josh@joshpigford.com>
2020-02-07 13:22:24 -0600

    🤦‍♂️

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,6 @@ require "sidekiq/web"
 
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["sidekiq_username"])) &
-  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["sidekiq_username"]))
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["sidekiq_password"]))
 end if Rails.env.production?
 
