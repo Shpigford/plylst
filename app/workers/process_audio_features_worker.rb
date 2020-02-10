2579d3c8dde3082e49e224db66b91ba8d08a2348
Josh Pigford <josh@joshpigford.com>
2020-02-10 15:32:42 -0600

    Reduce this to potentially help with URITooLong errors

--- app/workers/process_audio_features_worker.rb
+++ app/workers/process_audio_features_worker.rb
@@ -2,7 +2,7 @@ class ProcessAudioFeaturesWorker
   include Sidekiq::Worker
 
   def perform
-    Track.where(key: nil).where('audio_features_last_checked < ? OR audio_features_last_checked IS NULL', 72.hours.ago).pluck(:spotify_id).each_slice(100) do |slice|
+    Track.where(key: nil).where('audio_features_last_checked < ? OR audio_features_last_checked IS NULL', 72.hours.ago).pluck(:spotify_id).each_slice(90) do |slice|
       Track.where(spotify_id: slice).update_all(audio_features_last_checked: Time.now)
       AudioFeaturesWorker.set(queue: :slow).perform_async(slice)
     end
