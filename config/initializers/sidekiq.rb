accbefd5b6a126c6f58735fef774d331ac16830a
Josh Pigford <josh@joshpigford.com>
2020-02-10 15:26:30 -0600

    Set max retries here as well

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,5 @@ Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["sidekiq_password"]))
 end if Rails.env.production?
 
+
+Sidekiq.options[:max_retries] = 10
