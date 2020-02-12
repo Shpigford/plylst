72a34af46bede32fc0b11354bb53a40f0508a9c2
Josh Pigford <josh@joshpigford.com>
2020-02-12 15:48:15 -0600

    Array didn't do what I thought it would

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
     end
 
     if self.genres.present?
-      (Array(self.genres) + pop_genres).uniq.sort_by(&:downcase)
+      (JSON.parse(self.genres) + pop_genres).uniq.sort_by(&:downcase)
     else
       pop_genres.uniq.sort_by(&:downcase)
     end
