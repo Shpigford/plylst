f4ba7eb61fafc972c4893e347d636c26a670f160
Josh Pigford <josh@joshpigford.com>
2020-02-18 16:49:35 -0600

    Hide sidebar and don't cache while we work out the kinks

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -5,5 +5,6 @@ class LabsController < ApplicationController
 
   def most_listened_tracks
     @tracks = Follow.where('plays > 0').group(:track_id, :id).select('SUM(plays) as total_plays', :track_id).includes(:track).order('total_plays DESC').limit(100)
+    @hide_sidebar = true
   end
 end
