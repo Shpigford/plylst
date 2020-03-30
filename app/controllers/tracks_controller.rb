ba4b04925745e2c5a0a376c06bf0984efe40f941
Josh Pigford <josh@joshpigford.com>
2020-03-30 11:46:43 -0500

    Allow track blocking

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,14 @@
 class TracksController < ApplicationController
+  def destroy
+    track = Track.find(params[:id])
+    followed_track = current_user.follows.find_by(track: track)
+    followed_track.update_attribute(:active, false) if followed_track.present?
+
+    redirect_to playlist_path(params[:playlist_id]), notice: "Blocked that track for you!"
+  end
+
+  private
+  def track_params
+    params.require(:track).permit(:user, :active, :playlist)
+  end
 end
