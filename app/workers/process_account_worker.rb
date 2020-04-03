edbfaec30816f85a28c78ea21bd5b0858a769ac8
Josh Pigford <josh@joshpigford.com>
2020-04-03 10:04:23 -0500

    Dont process liked albums
    This adds massive overhead and seems to confuse more than help

--- app/workers/process_account_worker.rb
+++ app/workers/process_account_worker.rb
@@ -19,7 +19,7 @@ class ProcessAccountWorker
         ProcessTracksWorker.perform_async(user.id, n)
       end
       
-      ProcessAlbumsWorker.set(queue: :slow).perform_async(user.id)
+      #ProcessAlbumsWorker.set(queue: :slow).perform_async(user.id)
       ProcessPlaylistsWorker.set(queue: :slow).perform_async(user.id)
       BuildUserGenresWorker.set(queue: :critical).perform_in(30.seconds, user.id)
       UpdatePlayDataWorker.perform_in(60.seconds, user.id)
