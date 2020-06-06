20801809b1110bf0d261ad1a20c2ca7d39e5a678
Zach McCormick <zachary.tyler.mccormick@gmail.com>
2020-06-06 13:42:43 -0400

    Add ScoutAPM. Tell Puma to pre-load classes to save memory usage

--- Gemfile
+++ Gemfile
@@ -58,6 +58,9 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'dalli'
 
+# Add Scout for APM support
+gem 'scout_apm'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- config/puma.rb
+++ config/puma.rb
@@ -32,7 +32,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
